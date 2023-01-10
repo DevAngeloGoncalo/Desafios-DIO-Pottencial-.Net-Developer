39e11f5449471450bbde02ea074c1e4e585c2b02
DevAngeloGoncalo <angelo.goncalo87@gmail.com>
2023-01-10 18:45:56 -0300

    Initial commit

---
+++ Desafio1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace desafio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int n = int.Parse(Console.ReadLine());
+            int n = 234;
+            int p = 1, s = 0;
+            while (n > 0)
+            {
+                int l = n % 10;
+                // TODO: Crie as outras condições necessárias para a resolução do desafio:
+                p *= l;
+                s += l;
+                n /= 10;
+            }
+            Console.WriteLine("aa");
+            Console.WriteLine(p-s);
+        }
+    }
+}
---
+++ Desafio2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Desafio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int qt = int.Parse(Console.ReadLine());
+            // TODO: Crie as outras condições necessárias para a resolução do desafio:
+            string[] v = new string[2];
+
+
+        for (int contador= 0; contador < qt; contador++)
+            {
+                v = Console.ReadLine().Split(" ");
+                string a = v[0];
+                string b = v[1];
+
+                if (b.Length > a.Length)
+                    Console.WriteLine("nao encaixa");
+                else if (a.EndsWith(b))
+                    Console.WriteLine("encaixa");
+                else
+                    Console.WriteLine("nao encaixa");
+
+            }
+        }
+    }
+}
