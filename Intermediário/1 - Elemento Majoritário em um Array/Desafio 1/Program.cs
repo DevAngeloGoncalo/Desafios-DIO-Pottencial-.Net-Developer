e2339149578628c7b7109e3104f274c53c2be21b
DevAngeloGoncalo <angelo.goncalo87@gmail.com>
2023-01-14 17:35:11 -0300

    Desafio Elemento Majoritário

---
+++ Intermediário/1 - Elemento Majoritário em um Array/Desafio 1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Desafio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] num = new int[n];
+
+            // TODO: Crie as outras condições necessárias para a resolução do desafio:
+            for (int i = 0; i < n; i++)
+            {
+                num[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(MajorityElement(num));
+            
+        }
+
+        public static int MajorityElement(int[] nums)
+        {
+            int major = nums[0];
+            int count = 1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (count == 0)
+                {
+                    major = nums[i];
+                }
+                else
+                {
+                    if (major == nums[i])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        count--;
+                    }
+                }
+            }
+            return major;
+        }
+    }
+}
